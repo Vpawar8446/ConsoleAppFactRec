9d24b67ca43137c9ffdb65c1f18fec795a9b0710
niles <niles@LAPTOP-3K24CQ1R>
2021-09-01 22:32:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleAppFactRec
+{
+    class Recursive
+    {
+        internal int Fact(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            return n * Fact(n - 1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.Write("Enter the number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Recursive obj = new Recursive();
+            int num=obj.Fact(n);
+            Console.WriteLine("Factorial of given number is " + n + "! = " + num);
+
+        }
+    }
+}
